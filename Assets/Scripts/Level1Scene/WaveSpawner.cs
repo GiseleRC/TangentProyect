4909a5443b762e1d75746a6ca7fc2fac262d89b7
Gisele R. Caamaño <88439393+GiseleRC@users.noreply.github.com>
2023-09-25 17:16:46 -0300

    Add wave spawn manager

---
+++ Assets/Scripts/Level1Scene/WaveSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform _enemyPrefab;
+    public Transform _spawnPoint;
+    public float _timeBetweenWaves = 5f;
+
+    private float _countdown = 2f;
+    private int _waveIndex = 0;
+
+    void Update()
+    {
+        if (_countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            _countdown = _timeBetweenWaves;
+        }
+
+        _countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        _waveIndex++;
+        for (int i = 0; i < _waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(_enemyPrefab, _spawnPoint.position, _spawnPoint.rotation);
+    }
+}
