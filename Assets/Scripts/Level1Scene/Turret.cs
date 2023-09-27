f035893045d09180250413220e2b32cd1c098137
Gisele R. Caamaño <rominagisele.ar@gmail.com>
2023-09-26 22:17:26 -0300

    add detection and rotation of turrets

--- Assets/Scripts/Level1Scene/Turret.cs
+++ Assets/Scripts/Level1Scene/Turret.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform _target;
+    public Transform _partToRotate;
+    public float _range = 15f;
+    public float _turnSpeed = 10f;
+    public string _enemyTag = "Enemy";
+
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
-    // Update is called once per frame
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(_enemyTag);
+        GameObject nearestEnemy = null;
+        float shortesDistance = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortesDistance)
+            {
+                shortesDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortesDistance <= _range)
+        {
+            _target = nearestEnemy.transform;
+        }
+        else
+        {
+            _target = null;
+        }
+    }
+
     void Update()
     {
-        
+        if (_target == null)
+            return;
+
+        Vector3 dir = _target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(_partToRotate.rotation, lookRotation, Time.deltaTime * _turnSpeed).eulerAngles;
+        _partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _range);
     }
 }
