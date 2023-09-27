aadc8592b234f556e42536d86385adeaacb02673
Gisele R. Caamaño <rominagisele.ar@gmail.com>
2023-09-27 15:09:26 -0300

    Add arrow an effect impact

---
+++ Assets/Scripts/Level1Scene/Arrow.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Transform target;
+
+    public float _speed = 70f;
+    public GameObject _impactEffectPrefab;
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = _speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        GameObject effectGO = (GameObject)Instantiate(_impactEffectPrefab, transform.position, transform.rotation);
+        Destroy(effectGO, 2f);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Level1Scene/Turret.cs
+++ Assets/Scripts/Level1Scene/Turret.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform _target;
-    public Transform _partToRotate;
+
+    [Header("***Attributes***")]
+
     public float _range = 15f;
-    public float _turnSpeed = 10f;
+    public float _fireRate = 1f;
+    private float _fireCountdown = 0f;
+
+    [Header("***Unity Setups Fields***")]
+
     public string _enemyTag = "Enemy";
+    public float _turnSpeed = 10f;
+    public Transform _partToRotate;
+    public Transform _firePoint;
+    public GameObject _arrowPrefab;
 
     void Start()
     {
@@ -50,6 +60,26 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(_partToRotate.rotation, lookRotation, Time.deltaTime * _turnSpeed).eulerAngles;
         _partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(_fireCountdown <= 0f)
+        {
+            Shoot();
+            _fireCountdown = 1f / _fireRate;
+
+        }
+
+        _fireCountdown -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        GameObject arrowGO = (GameObject)Instantiate(_arrowPrefab, _firePoint.position, _firePoint.rotation);
+        Arrow arrow = arrowGO.GetComponent<Arrow>();
+
+        if (arrow != null)
+        {
+            arrow.Seek(_target);
+        }
     }
 
     private void OnDrawGizmosSelected()
