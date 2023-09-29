0e71d9e5b68f1bb9d53c95f71a71a8b98e93b5ce
Astrogodo <nicolasplc@hotmail.com>
2023-09-29 20:40:28 -0300

    Defeat
    The scene reloads when enemies reach the base

--- Assets/Scripts/Level1Scene/Enemy.cs
+++ Assets/Scripts/Level1Scene/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
@@ -30,6 +31,7 @@ public class Enemy : MonoBehaviour
         if(_waypointsIndex >= Waypoints.points.Length -1)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             return;
         }
         _waypointsIndex++;
