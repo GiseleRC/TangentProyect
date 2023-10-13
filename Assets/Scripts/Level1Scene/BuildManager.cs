d01ccf21cdd91d792ccf009d818d0a368b8bafd1
Gisele R. Caamaño <88439393+GiseleRC@users.noreply.github.com>
2023-10-13 02:20:48 -0300

    Upgrade

--- Assets/Scripts/Level1Scene/BuildManager.cs
+++ Assets/Scripts/Level1Scene/BuildManager.cs
@@ -21,6 +21,12 @@ public class BuildManager : MonoBehaviour
 
     public void BuildTurretOn(Node node)
     {
+        if (PlayerStats.Money < _turretToBuild.cost)
+        {
+            return;
+        }
+        PlayerStats.Money -= _turretToBuild.cost;
+
         GameObject turret = (GameObject)Instantiate(_turretToBuild.prefab, node.transform.position + node.positionOffset, node.transform.rotation);
         node.turret = turret;
     }
--- Assets/Scripts/Level1Scene/PlayerStats.cs
+++ Assets/Scripts/Level1Scene/PlayerStats.cs
@@ -1,16 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static int Money;
+    public int startMoney = 500;
     void Start()
     {
-        
+        Money = startMoney;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
