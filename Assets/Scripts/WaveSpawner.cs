85636b995d5d923285d98dab8ee07858504c8866
Gisele R. Caamaño <88439393+GiseleRC@users.noreply.github.com>
2023-10-18 00:24:32 -0300

    Some lines out

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -5,9 +5,8 @@ using TMPro;
 
 public class WaveSpawner : MonoBehaviour
 {
-    //[SerializeField] private Transform _spawnPoint;
     [SerializeField] private float _timeBetweenWaves = 5f;
-    private float _countdown = 2f;// HACER --------- usarlo para un slide donde se vea como va descontandose el tiempo antes de la siguiente wave
+    private float _countdown = 2f;
     private int _waveIndex = 0;
     private float _secondsToWaitWave = 0.5f;
     private string _waveText = "Wave: ";
@@ -26,7 +25,6 @@ public class WaveSpawner : MonoBehaviour
     void Update()
     {
         StartWave();
-        //_countdown -= Time.deltaTime;
     }
 
     private void StartWave()
