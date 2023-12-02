caf2fbc79af5efad797a8f4c9f6600350136987c
Matias Lozada <matias.lozada@nimblegiant.com>
2023-12-01 21:24:25 -0300

    text descripttion

--- Assets/Scripts/Decorator/TurretDecoratorDebugger.cs
+++ Assets/Scripts/Decorator/TurretDecoratorDebugger.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
 
 public class TurretDecoratorDebugger : MonoBehaviour
 {
     [SerializeField] private GameObject _panelButton;
+    [SerializeField] private TextMeshPro _description;
 
+    private void Start()
+    {
+        _description.gameObject.SetActive(true);
+
+        Invoke("TextOff", 3f);
+    }
     public void GetSimpleIceShoot(Turret _myTurret)
     {
         _myTurret = new SimpleIceShoot(_myTurret);
         _myTurret.ImpactEffect();
-        _myTurret.GetDescription();
         _myTurret.AreaHit();
 
-        Debug.Log(" " + _myTurret.GetDescription() + " " + _myTurret.AreaHit() + " ");
+        _description.text = _myTurret.GetDescription();
         _panelButton.SetActive(false);
     }
 
@@ -22,15 +29,20 @@ public class TurretDecoratorDebugger : MonoBehaviour
     {
         _myTurret = new DoubleFireShoot(_myTurret);
         _myTurret.ImpactEffect();
-        _myTurret.GetDescription();
         _myTurret.AreaHit();
 
-        Debug.Log(" " + _myTurret.GetDescription() + " " + _myTurret.AreaHit() + " ");
+        _description.text = _myTurret.GetDescription();
         _panelButton.SetActive(false);
     }
 
     private void OnMouseDown()
     {
         _panelButton.SetActive(true);
+    }
+
+    private void TextOff()
+    {
+        _description.gameObject.SetActive(false);
+        _description.text = " ";
     }
 }
